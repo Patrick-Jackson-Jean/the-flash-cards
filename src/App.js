325e6139189a54290590f65488981c547a011002
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-08-12 15:25:36 -0400

    Added logic for ListCard (home) component. Changed rendering to route based. Added edit logic inside FlashCard. Temp removed backend.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,54 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import FlashCard from './components/FlashCard/Flashcard';
-import ListCards from './components/ListCards/ListCards';
-import EditCard from './components/EditCard/EditCard';
-import CreateCard from './components/CreateCard/CreateCard';
-import NewCardsForm from './components/NewCardsForm/NewCardsForm';
+import "./App.css";
+import React, { useState } from "react";
+import FlashCard from "./components/FlashCard/FlashCard";
+import ListCards from "./components/ListCards/ListCards";
+import EditCard from "./components/EditCard/EditCard";
+import NewCardsForm from "./components/NewCardsForm/NewCardsForm";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [newCards, setNewCards] = useState([
+    { id: 1, front: "1 x 1 = ?", back: "1" },
+    { id: 2, front: "2 x 2 = ?", back: "4" },
+    { id: 3, front: "3 x 3 = ?", back: "9" },
+  ]);
 
-  const [newFormOpen, setNewFormOpen] = useState(false)
-  const [newCards, setNewCards] = useState([])
-
-  const changeFormVisibility = () => {
-    setNewFormOpen(!newFormOpen)
-  }
   const submitCards = (cards, e) => {
-    e.preventDefault()
-    console.log(cards)
-    setNewCards([...newCards, cards])
-    changeFormVisibility()
+    e.preventDefault();
+    console.log(cards);
+    setNewCards([...newCards, cards]);
+  };
+
+  const submitEdit = (card, e) => {
+    e.preventDefault();
+    const cardToEdit = newCards.findIndex((c) => c.id === card.id);
+    const newCardUpdate = newCards;
+    newCardUpdate[cardToEdit] = card;
+    setNewCards(newCardUpdate);
+    console.log(card.id)
   }
-
-  const [decks, setDecks] = useState([]);
-
-  useEffect(() => {
-    async function getDecks() {
-      const response = await fetch("http://localhost:5000/decks", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      setDecks(data);
-    }
-    getDecks();
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Flash Cards</h1>
-        <button onClick={changeFormVisibility}>Add Cards</button>
-        <Router>
-          <Routes>
-            <Route path="/" element={<ListCards decks={decks} />} />
-            <Route path="/EditCard" element={<EditCard />} />
-            <Route path="/CreateCard" element={<CreateCard />} />
-          </Routes>
-        </Router>
-      </header>
-        {newFormOpen && <NewCardsForm closeNewForm={changeFormVisibility} submitCards={submitCards}/>}
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link className="header-link" to="/">
+            <h1>The Flash Cards</h1>
+          </Link>
+          <Link className="header-link" to="/CreateCards">
+            <p>Create New Cards</p>
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<ListCards cards={newCards} submitEdit={submitEdit} />} />
+          <Route path="/EditCard" element={<EditCard />} />
+          <Route
+            path="/CreateCards"
+            element={<NewCardsForm submitCards={submitCards} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/FlashCard/Flashcard.js
+++ src/components/FlashCard/Flashcard.js
@@ -1,18 +1,55 @@
 import "./FlashCard.css";
 import React, { useState } from "react";
+import { FiEdit } from "react-icons/fi";
 
-export default function FlashCard(card) {
+export default function FlashCard({ front, back, id, edit }) {
   const [isFlipped, setIsFlipped] = useState(false);
-
+  const [isEdit, setIsEdit] = useState(false);
+  const [newCard, setNewCard] = useState({ front, back, id });
+  console.log(front, back);
   // if the card is flipped, show the back of the card
   // otherwise, show the front of the card
 
+  const notEditing = (
+    <div className="card-body">
+      <h5 className="card-front">{!isFlipped ? front : back}</h5>
+    </div>
+  );
+
+  const handleEdit = (e) => {
+    e.target.name === "front"
+      ? setNewCard({ ...newCard, front: e.target.value })
+      : setNewCard({ ...newCard, back: e.target.value });
+  };
+
+  const beingEdited = (
+    <div className="card-body">
+      <form onSubmit={(e) => edit(newCard, e)}>
+      <h5 className="card-front">
+        <input
+          type="text"
+          name="front"
+          placeholder={front}
+          onChange={(e) => handleEdit(e)}
+        />
+      </h5>
+      <h5 className="card-back">
+        <input
+          type="text"
+          name="back"
+          placeholder={back}
+          onChange={(e) => handleEdit(e)}
+        />
+      </h5>
+      <button>Submit</button>
+      </form>
+    </div>
+  );
+
   return (
-    <div className="card" onClick={setIsFlipped(!isFlipped)}>
-      <div className="card-body">
-        <p className="card-text">Hello!</p>
-        <h5 className="card-front">{!isFlipped ? card.front : card.back}</h5>
-      </div>
+    <div className="card" onClick={() => setIsFlipped(!isFlipped)}>
+      <FiEdit onClick={() => setIsEdit(!isEdit)} />
+      {!isEdit ? notEditing : beingEdited}
     </div>
   );
 }
