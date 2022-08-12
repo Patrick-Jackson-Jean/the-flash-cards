424542be052145ce8717a4e150ca47f436ec7a0b
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-08-12 12:53:40 -0400

    combining patrick and Jackson's version via pull to main

--- src/components/FlashCard/Flashcard.js
+++ src/components/FlashCard/Flashcard.js
@@ -1,36 +1,17 @@
 import "./FlashCard.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-export default function FlashCard() {
-  const [deck, setDeck] = useState([]);
-  const [card, setCard] = useState({});
-  const [cardIndex, setCardIndex] = useState(0);
+export default function FlashCard(card) {
   const [isFlipped, setIsFlipped] = useState(false);
 
-  useEffect(() => {
-    async function getDeck() {
-      const response = await fetch(
-        "http://localhost:5000/decks",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const data = await response.json();
-      setDeck(data);
-      setCard(data[cardIndex]);
-    }
-    getDeck();
-  }, [cardIndex]);
+  // if the card is flipped, show the back of the card
+  // otherwise, show the front of the card
 
   return (
-    <div className="card">
+    <div className="card" onClick={setIsFlipped(!isFlipped)}>
       <div className="card-body">
         <p className="card-text">Hello!</p>
-        <h5 className="card-front">{card.front}</h5>
-        <h5 className="card-back">{card.back}</h5>
+        <h5 className="card-front">{!isFlipped ? card.front : card.back}</h5>
       </div>
     </div>
   );
